9806bb954d900166c528da6e083b6f935432148a
Danillo Moraes <danillo.moraes@outlook.com>
2019-04-20 14:32:24 -0300

    Finalizado app bar proto

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new BlurViewPackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage()
--- components/ui-components/navbar.js
+++ components/ui-components/navbar.js
@@ -1,70 +1,82 @@
 import React, { Component } from "react";
 import { View, StyleSheet, StatusBar, Text } from "react-native"
 import { Gradient } from "./gradient";
-
+import FontAwesome, { Icons, IconTypes } from 'react-native-fontawesome'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class NavBar extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    render() {
-        return (
-            <View style={stylef.navbar}>
-                <StatusBar translucent={true} backgroundColor={'rgba(0,0,0,0)'}></StatusBar>
-                <Gradient style={stylef.gradient} colors={["#FA405F", '#D540B3']}>
-                    <View style={stylef.content}>
-                        <View style={stylef.menuButton}>
-                            <Text>btn</Text>
-                        </View>
-                        <View style={stylef.middle}>
-                            <Text style={stylef.text}>It's just works!</Text>
-                        </View>
-                        <View style={stylef.userAvatar}>
-                            <Text>faustao</Text>
-                        </View>
-                    </View>
-                </Gradient>
+  render() {
+    return (
+      <View style={stylef.navbar}>
+        <StatusBar translucent={true} backgroundColor={'rgba(0,0,0,0)'}></StatusBar>
+        <Gradient style={stylef.gradient} colors={["#FA405F", '#D540B3']}>
+          <View style={stylef.content}>
+            <View style={stylef.viewButton}>
+              <View style={stylef.menuButton}>
+                {/* <FontAwesome class={stylef.menuIcon}>{Icons.batteryEmpty}</FontAwesome>  */}
+                <Icon style={stylef.menuIcon} name="bars"></Icon>
+              </View>
             </View>
-        )
-    }
+            <View style={stylef.middle}>
+              <Text style={stylef.text}>Home</Text>
+            </View>
+            <View style={stylef.searchIcon}>
+              <Icon style={stylef.menuIcon} name="search"></Icon>
+            </View>
+          </View>
+        </Gradient>
+      </View>
+    )
+  }
 
-} 
+}
 const stylef = StyleSheet.create({
-    navbar: {
-        height: 70,
-        shadowColor: '#000000',
-        shadowOffset: {
-            width: 10,
-            height: 10
-        },
-        shadowOpacity: 0.5,
-        shadowRadius: 10,
-        elevation: 1,
+  navbar: {
+    height: 70,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 10,
+      height: 10
     },
-    gradient: {
-        flex:1,
-    },
-    content: {
-        flex: 1,
-        marginTop:20,
-        flexDirection:'row',
-        alignItems: 'center'  
-    },
-    text: {
-        textAlign:'center'
-    },
-    menuButton:{
-        textAlign:'center',
-        width:50,
-        backgroundColor: '#FFFFFF'
-    },
-    userAvatar:{
-        textAlign:'center',
-        width:50,
-        backgroundColor: '#FFFFFF'
-    },
-    middle: {
-        flex:1
-    }
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    elevation: 1,
+  },
+  gradient: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  text: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: '600'
+  },
+  menuButton: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  menuIcon: {
+    color: '#FFFFFF',
+    fontSize: 20
+  },
+  viewButton: {
+    width: 50,
+  },
+  searchIcon: {
+    width: 50,
+    alignItems:'center'
+  },
+  middle: {
+    flex: 1
+  }
 })
